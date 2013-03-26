8b9d5376d9b8c9f18d894f91adabbe1b5602690e
Steven Erat <erat@adobe.com>
2013-03-26 17:17:11 -0400

    Adding qUnit support.  Cleaning up some linting nits.

---
+++ samples/qunit_test.js
@@ -0,0 +1,13 @@
+/* brackets-xunit: qunit */
+
+test( "My First Test", function() {
+    ok( 1 == "1", "Warm and fuzzies" );
+});
+
+test( "My Second Test", function() {
+    ok( 2 == "2", "Still warm" );
+});
+
+test( "My Third Test", function() {
+    ok( 3 == "3", "Still fuzzy" );
+});

06daf911b4c7743551aa4bc65e316573dd9e1fe3
Dan Schaffer <dschaffe@adobe.com>
2013-04-09 15:33:26 -0400

    Issue 17: Generate jasmine test template for the current file

---
+++ samples/simpleapi/mathfns.js
@@ -0,0 +1,28 @@
+function fibonacci(n) {
+    'use strict';
+    if (n < 2) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+function factorial(n) {
+    'use strict';
+    if (n < 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+function choose(m, n) {
+    'use strict';
+    return factorial(m) / factorial(m - n) / factorial(n);
+}
+function birthday(n) {
+    'use strict';
+    var result = 1, i;
+    for (i = 0; i < n; i += 1) {
+        result = result * (365 - i) / 365;
+    }
+    return 1 - result;
+}
